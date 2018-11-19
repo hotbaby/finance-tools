7201499089f3db0118a6f68ddb5aa219db99904c
yy <mengyy_linux@163.com>
2018-11-19 17:55:06 +0800

    Add amount num2str function

--- fin_tools/utils.py
+++ fin_tools/utils.py
@@ -25,3 +25,23 @@ def finance_eaoi_annual_rate(loan, repayment, total_index):
     :return 年化利率
     """
     return finance_eaoi_irr(loan, repayment, total_index) * 12
+
+
+def finance_num2str(amount):
+    """
+    金额转换成字符串
+    :param amount: 金额（元）
+    :return amount_str
+    """
+    amount_integer, amount_decial = str(float(amount)).split('.')
+    amount_integer = int(amount_integer)
+    amount_decial = amount_decial
+    amount_integer_split_list = []
+
+    while amount_integer:
+        s = str(amount_integer % 1000)
+        amount_integer /= 1000
+        s = s.zfill(3) if amount_integer else s
+        amount_integer_split_list.append(s)
+
+    return '%s.%s' % (','.join(amount_integer_split_list[::-1]), amount_decial)
